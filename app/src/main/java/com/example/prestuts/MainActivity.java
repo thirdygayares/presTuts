ef60dc372a9474ff028a28d8f36f2653164730fd
thirdy454 <gayares@gmail.com>
2022-10-15 21:55:38 +0800

    relative layout

--- app/src/main/java/com/example/prestuts/MainActivity.java
+++ app/src/main/java/com/example/prestuts/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //ronnielyn valenzuela
+         //ronnielyn valenzuela
         //all is are above
     }
 }
